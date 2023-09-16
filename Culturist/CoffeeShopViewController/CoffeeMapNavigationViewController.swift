9307eb3e8f65961937b5651ab5b70012d837fa62
Peiyun <b612starnight@gmail.com>
2023-09-16 22:01:28 +0800

    fix map bug

--- Culturist/CoffeeShopViewController/CoffeeMapNavigationViewController.swift
+++ Culturist/CoffeeShopViewController/CoffeeMapNavigationViewController.swift
@@ -4,9 +4,9 @@ import CoreLocation
 
 class CoffeeMapNavigationViewController: UIViewController {
     
-    var latitude: Double?
-    var longitude: Double?
-    var name: String?
+    var latitude: Double!
+    var longitude: Double!
+    var name: String!
     
     var route: MKRoute?
     let mapView = MKMapView()
@@ -26,8 +26,6 @@ class CoffeeMapNavigationViewController: UIViewController {
         closeButton.setTitle("Close", for: .normal)
         closeButton.addTarget(self, action: #selector(closeNavigation), for: .touchUpInside)
         view.addSubview(closeButton)
-        
-        setDirectionsRequest()
         setAutolayout()
         
     }
@@ -38,15 +36,21 @@ class CoffeeMapNavigationViewController: UIViewController {
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        mapView.delegate = self
+        mapView.mapType = .mutedStandard
+        mapView.showsUserLocation = true
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         locationManager.stopUpdatingLocation()
     }
+    
+    @objc func closeNavigation() {
+        // close map
+        self.dismiss(animated: true, completion: nil)
+    }
 }
-
-
 
 // MARK: - MKMapViewDelegate
 extension CoffeeMapNavigationViewController: MKMapViewDelegate {
@@ -60,78 +64,67 @@ extension CoffeeMapNavigationViewController: MKMapViewDelegate {
         }
         return MKOverlayRenderer()
     }
-    
-    func setDirectionsRequest() {
-        // Get the user's current location
-        if let userLocation = locationManager.location {
-            // Use the user's current location as the center of the map
-            let coordinateRegion = MKCoordinateRegion(
-                center: userLocation.coordinate,
-                latitudinalMeters: 500,
-                longitudinalMeters: 500
-            )
-            mapView.setRegion(coordinateRegion, animated: true)
-        }
-        mapView.delegate = self
-        mapView.mapType = .mutedStandard
-        mapView.showsUserLocation = true
-        // Set the destination location using latitude and longitude
-        destinationLocation = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
-        let destinationAnnotation = MKPointAnnotation()
-        destinationAnnotation.coordinate = destinationLocation
-        destinationAnnotation.title = "\(name)"
-        mapView.addAnnotation(destinationAnnotation)
-        
-        // Create a placemark for the source (current location)
-        let sourcePlacemark = MKPlacemark(coordinate: locationManager.location?.coordinate ?? CLLocationCoordinate2D())
-        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
-        
-        // Create a placemark for the destination
-        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation)
-        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
-        
-        // Create a directions request
-        let directionsRequest = MKDirections.Request()
-        directionsRequest.source = sourceMapItem
-        directionsRequest.destination = destinationMapItem
-        directionsRequest.transportType = .automobile // You can choose the transportation type as needed
-        
-        // Calculate directions
-        let directions = MKDirections(request: directionsRequest)
-        directions.calculate { [self] (response, error) in
-            if let error = error {
-                print("Route calculation error: \(error.localizedDescription)")
-                return
-            }
-            if let route = response?.routes.first {
-                mapView.addOverlay(route.polyline, level: .aboveRoads)
-                // Save route information for later use
-                self.route = route
-                // Start navigation
-                mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
-            }
-        }
-    }
 }
 
 // MARK: - CLLocationManagerDelegate
 extension CoffeeMapNavigationViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
+        recalculateRoute()
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Location manager error: \(error.localizedDescription)")
     }
+    
+    func recalculateRoute() {
+        guard let userLocation = locationManager.location else {
+            print("無法獲取用戶位置")
+            return
+        }
+        // Set the destination location using latitude and longitude
+        // destinationLocation = CLLocationCoordinate2D(latitude: 37.32017, longitude: -122.04516)
+        destinationLocation = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let destinationAnnotation = MKPointAnnotation()
+        destinationAnnotation.coordinate = destinationLocation
+        destinationAnnotation.title = "\(name!)"
+        mapView.addAnnotation(destinationAnnotation)
+        
+        // Create a placemark for the source (current location)
+        let sourcePlacemark = MKPlacemark(coordinate: userLocation.coordinate)
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+
+        // Create a placemark for the destination
+        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+
+        // Create a directions request
+        let directionsRequest = MKDirections.Request()
+        directionsRequest.source = sourceMapItem
+        directionsRequest.destination = destinationMapItem
+        directionsRequest.transportType = .walking
+
+        // Calculate directions
+        let directions = MKDirections(request: directionsRequest)
+        directions.calculate { [weak self] (response, error) in
+            if let route = response?.routes.first {
+                // remove last route
+                self?.mapView.removeOverlays(self?.mapView.overlays ?? [])
+                
+                // add new route
+                self?.mapView.addOverlay(route.polyline, level: .aboveRoads)
+                self?.route = route
+                
+                // Start navigation
+                self?.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+
+            }
+        }
+    }
+
 }
 
 // MARK: - extension
 extension CoffeeMapNavigationViewController {
-    
-    @objc func closeNavigation() {
-        // close map
-        self.dismiss(animated: true, completion: nil)
-    }
     
     func setAutolayout() {
         // Add auto-layout constraints for the mapView
