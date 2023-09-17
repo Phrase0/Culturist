7244e11dd5c7170c94a8bf877e1efeef363ecf75
Peiyun <b612starnight@gmail.com>
2023-09-17 12:35:51 +0800

    simple UI

---
+++ Culturist/RecommendViewController/RecommendCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendCollectionViewCell.swift
+//  Culturist
+//
+//  Created by Peiyun on 2023/9/17.
+//
+
+import UIKit
+
+class RecommendCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descripLabel: UILabel!
+    
+}
