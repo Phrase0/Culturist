928f0b1bacf03156d733643ec6d3cde4cdee45eb
Peiyun <b612starnight@gmail.com>
2024-03-06 21:27:10 +0800

    feat:mapVC

---
+++ Culturist/Views/Map/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  Culturist
+//
+//  Created by Peiyun on 2024/3/6.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
